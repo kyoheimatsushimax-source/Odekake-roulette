41c20428a5a5cda22b2c422b81ca35c73531658e
kyoheimatsushimax-source <kyoheimatsushimax@gmail.com>
2026-07-11 10:35:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "odekake-v1";
+const CACHE = "odekake-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 
 self.addEventListener("install", e => {
